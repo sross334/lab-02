2bd5111b03f049da5d8432633bce009c3285d08f
sross334 <sross4@ualberta.ca>
2026-01-15 22:06:15 -0700

    added button functionality

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.listycity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -18,9 +25,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    ListView cityList;
-    ArrayAdapter<String> cityAdapter;
-    ArrayList<String> dataList;
+    private ListView cityList;
+    private ArrayAdapter<String> cityAdapter;
+    private ArrayList<String> dataList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +41,47 @@ public class MainActivity extends AppCompatActivity {
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
 
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, dataList);
         cityList.setAdapter(cityAdapter);
+        Button add = findViewById(R.id.add);
+        Button confirm = findViewById(R.id.confirm);
+        Button delete = findViewById(R.id.delete);
+        EditText editCity = findViewById(R.id.type_city);
 
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = cityList.getCheckedItemPosition();
+                if(pos != ListView.INVALID_POSITION) {
+                    dataList.remove(pos);
+                }
+                    cityList.clearChoices();
+                    cityAdapter.notifyDataSetChanged();
+            }
+        });
 
-    }
-}
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                editCity.setVisibility(View.VISIBLE);
+                confirm.setVisibility(View.VISIBLE);
+                editCity.requestFocus();
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = editCity.getText().toString();
+                if(!city.isEmpty()){
+                    dataList.add(city);
+                    cityAdapter.notifyDataSetChanged();
+                    editCity.setText("");
+                    confirm.setVisibility(View.GONE);
+                    editCity.setVisibility(View.GONE);
+                    editCity.clearFocus();
+                }
+            }
+        });
+
+}}
